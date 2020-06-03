023a2b3086129281496b3e86d2d65f5d5aa9bd8e
Charly <charly.martin.1207@gmail.com>
2020-06-03 15:38:13 +0200

    Add count

--- pages/index.js
+++ pages/index.js
@@ -11,28 +11,38 @@ export default function Index() {
   // console.log(data);
 
   return (
-    <table style={{ textAlign: "left" }}>
-      <h2>Babs Stocks</h2>
-      <tr>
-        <th>Product</th>
-        <th>SKU</th>
-        <th>Stock</th>
-      </tr>
-      {products.map((product) => (
+    <div>
+      <table style={{ textAlign: "left" }}>
+        <h2>Babs Stocks</h2>
         <tr>
-          <td>
-            <p style={{ margin: "4px", marginRight: "32px" }}>{product.name}</p>
-          </td>
-          <td>
-            <p style={{ margin: "4px", marginRight: "32px" }}> {product.sku}</p>
-          </td>
-          <td>
-            <p style={{ margin: "4px", marginRight: "32px" }}>
-              {product.stock}
-            </p>
-          </td>
+          <th>Product</th>
+          <th>SKU</th>
+          <th>Stock</th>
         </tr>
-      ))}
-    </table>
+        {products.map((product) => (
+          <tr>
+            <td>
+              <p style={{ margin: "4px", marginRight: "32px" }}>
+                {product.name}
+              </p>
+            </td>
+            <td>
+              <p style={{ margin: "4px", marginRight: "32px" }}>
+                {" "}
+                {product.sku}
+              </p>
+            </td>
+            <td>
+              <p style={{ margin: "4px", marginRight: "32px" }}>
+                {product.stock}
+              </p>
+            </td>
+          </tr>
+        ))}
+      </table>
+      <i>
+        <p>BJ Count: 3</p>
+      </i>
+    </div>
   );
 }
